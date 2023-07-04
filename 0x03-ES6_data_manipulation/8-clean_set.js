1fab4a1660e045806a1f461bf579b0c8be3119a4
Yosinan <ymymym4634@gmail.com>
2023-07-04 17:42:03 +0300

    ES6_data_manipulation

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,7 +1,7 @@
 export default function cleanSet(set, startString) {
     if (typeof startString === 'string' && startString.length > 0) {
-        return [...set].filter((element) => element.startsWith(startString)).map((element) => element.slice(startString.length));
+        return [...set].filter((element) => element.startsWith(startString)).map((element) => element.slice(startString.length)).join('-');
     }
     
-    return [];
+    return '';
     }
